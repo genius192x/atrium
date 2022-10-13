2d9de0b1de9c4d82f12f11b65cae8dcdd9ab6f5f
genius192x <mishalevchenko161@gmail.com>
2022-10-13 21:00:41 +0300

    v2

--- js/app.js
+++ js/app.js
@@ -31,81 +31,17 @@
         hash = hash ? `#${hash}` : window.location.href.split("#")[0];
         history.pushState("", "", hash);
     }
-    let _slideUp = (target, duration = 500, showmore = 0) => {
-        if (!target.classList.contains("_slide")) {
-            target.classList.add("_slide");
-            target.style.transitionProperty = "height, margin, padding";
-            target.style.transitionDuration = duration + "ms";
-            target.style.height = `${target.offsetHeight}px`;
-            target.offsetHeight;
-            target.style.overflow = "hidden";
-            target.style.height = showmore ? `${showmore}px` : `0px`;
-            target.style.paddingTop = 0;
-            target.style.paddingBottom = 0;
-            target.style.marginTop = 0;
-            target.style.marginBottom = 0;
-            window.setTimeout((() => {
-                target.hidden = !showmore ? true : false;
-                !showmore ? target.style.removeProperty("height") : null;
-                target.style.removeProperty("padding-top");
-                target.style.removeProperty("padding-bottom");
-                target.style.removeProperty("margin-top");
-                target.style.removeProperty("margin-bottom");
-                !showmore ? target.style.removeProperty("overflow") : null;
-                target.style.removeProperty("transition-duration");
-                target.style.removeProperty("transition-property");
-                target.classList.remove("_slide");
-                document.dispatchEvent(new CustomEvent("slideUpDone", {
-                    detail: {
-                        target
-                    }
-                }));
-            }), duration);
-        }
-    };
-    let _slideDown = (target, duration = 500, showmore = 0) => {
-        if (!target.classList.contains("_slide")) {
-            target.classList.add("_slide");
-            target.hidden = target.hidden ? false : null;
-            showmore ? target.style.removeProperty("height") : null;
-            let height = target.offsetHeight;
-            target.style.overflow = "hidden";
-            target.style.height = showmore ? `${showmore}px` : `0px`;
-            target.style.paddingTop = 0;
-            target.style.paddingBottom = 0;
-            target.style.marginTop = 0;
-            target.style.marginBottom = 0;
-            target.offsetHeight;
-            target.style.transitionProperty = "height, margin, padding";
-            target.style.transitionDuration = duration + "ms";
-            target.style.height = height + "px";
-            target.style.removeProperty("padding-top");
-            target.style.removeProperty("padding-bottom");
-            target.style.removeProperty("margin-top");
-            target.style.removeProperty("margin-bottom");
-            window.setTimeout((() => {
-                target.style.removeProperty("height");
-                target.style.removeProperty("overflow");
-                target.style.removeProperty("transition-duration");
-                target.style.removeProperty("transition-property");
-                target.classList.remove("_slide");
-                document.dispatchEvent(new CustomEvent("slideDownDone", {
-                    detail: {
-                        target
-                    }
-                }));
-            }), duration);
-        }
-    };
     let bodyLockStatus = true;
     let bodyUnlock = (delay = 500) => {
-        document.querySelector("body");
+        let body = document.querySelector("body");
         if (bodyLockStatus) {
             let lock_padding = document.querySelectorAll("[data-lp]");
             setTimeout((() => {
                 for (let index = 0; index < lock_padding.length; index++) {
-                    lock_padding[index];
+                    const el = lock_padding[index];
+                    el.style.paddingRight = "0px";
                 }
+                body.style.paddingRight = "0px";
                 document.documentElement.classList.remove("lock");
             }), delay);
             bodyLockStatus = false;
@@ -3650,17 +3586,16 @@
         if (document.querySelector(".projects")) new core(".projects", {
             modules: [ Navigation ],
             observer: true,
-            observeParents: true,
+            observeParents: false,
             slidesPerView: "auto",
             spaceBetween: 24,
             speed: 1e3,
             loop: true,
-            loopedSlides: 2,
+            loopedSlides: 1,
             watchOverflow: true,
             loopAdditionalSlides: 5,
             preloadImages: false,
             parallax: true,
-            allowTouchMove: false,
             effect: "fade",
             autoplay: {
                 delay: 2e3,
@@ -3672,12 +3607,15 @@
             },
             breakpoints: {
                 320: {
+                    allowTouchMove: true,
                     spaceBetween: 20
                 },
                 530: {
                     spaceBetween: 20
                 },
+                768: {},
                 992: {
+                    allowTouchMove: false,
                     spaceBetween: 20
                 },
                 1268: {
@@ -3747,7 +3685,6 @@
             loopAdditionalSlides: 5,
             preloadImages: false,
             parallax: true,
-            allowTouchMove: false,
             effect: "fade",
             autoplay: {
                 delay: 2e3,
